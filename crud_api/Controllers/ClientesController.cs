09a460c9c71a75eefddb78648714683ea033d4bc
kelymbp <tester.kely@gmail.com>
2021-03-27 22:57:41 -0400

    Initial API with GET, POST and DELETE operations without a database

---
+++ crud_api/Controllers/ClientesController.cs
@@ -0,0 +1,33 @@
+﻿using crud_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace crud_api.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if(!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome));
+            }
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
