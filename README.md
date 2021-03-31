d7ad6f5ac90ea9178d09d8ec4de65c746d1b5304
kelymbp <tester.kely@gmail.com>
2021-03-31 02:20:46 -0400

    Added database and PUT request

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # crud_api
 This is a simple API to Create, Read, Update and Delete itens from database.
+
+This implementation doesn't have an interface, you can manipulate this just through the endpoints.
+
+You can open this solution in Visual Studio, run and use endpoints inside Postman, for example.
+
+The solution has only an entity named "Clientes".
+
+The available endpoints are:
+
+POST http://localhost:51688/api/Clientes?nome=<name>
+GET http://localhost:51688/api/Clientes
+DELETE http://localhost:51688/api/Clientes?nome=<name>
+PUT http://localhost:51688/api/Clientes?id=<number>&nome=<name>
+
+The database server it's set inside the project folder.
--- crud_api/Controllers/ClientesController.cs
+++ crud_api/Controllers/ClientesController.cs
@@ -5,29 +5,65 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace crud_api.Controllers
 {
     public class ClientesController : ApiController
     {
-        private static List<Cliente> clientes = new List<Cliente>();
+        //Identify database
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ClientesDB.mdf;Integrated Security=True");
 
-        public List<Cliente> Get()
+        //List all clients from database
+        public DataTable Get()
         {
-            return clientes;
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from table1";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
         }
 
+        //Add a client on database
         public void Post(string nome)
         {
             if(!string.IsNullOrEmpty(nome))
             {
-                clientes.Add(new Cliente(nome));
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into table1 values('"+nome+"')";
+                cmd.ExecuteNonQuery();
+                con.Close();
             }
         }
 
+        //Delete a client from database
         public void Delete(string nome)
         {
-            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from table1 where nome='" + nome + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        //Update a client on database
+        public void Put(string id, string nome)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update table1 set nome='" + nome + "' where id="+Convert.ToInt32(id)+"";
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
